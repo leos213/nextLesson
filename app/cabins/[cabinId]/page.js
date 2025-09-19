23bc3c2e7bef4a8fe193b95dcce5ae25248c701a
axali <l.sokhadze@gmail.com>
2025-09-19 16:19:26 +0400

    462

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,4 +1,4 @@
-import { getCabin } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
@@ -19,6 +19,14 @@ const cabin = {
 export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId);
   return { title: `Cabin ${name}` };
+}
+
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+  const ids = cabins.map((cabin) => ({
+    cabinId: String(cabin.id),
+  }));
+  return ids;
 }
 
 export default async function Page({ params }) {
@@ -49,7 +57,7 @@ export default async function Page({ params }) {
             <li className="flex gap-3 items-center">
               <UsersIcon className="h-5 w-5 text-primary-600" />
               <span className="text-lg">
-                For up to <span className="font-bold">{capacity.max}</span>{" "}
+                For up to <span className="font-bold">{maxCapacity}</span>{" "}
                 guests
               </span>
             </li>
--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,9 @@ const nextConfig = {
       },
     ],
   },
+  // 456 video: ეს გამოიყენება რომ მთელი აპლიკაცია იყოს სტატიკური, generateStaticParams-ს ეს ფუნქცია რომელიც გაწერილია [cabinId]
+  // ეუბნება რომ სტატიკურზე (html გვერდებად)გადაიყვანოს build-ის პროცესის დროს.
+  // output: "export",
 };
 
 export default nextConfig;
